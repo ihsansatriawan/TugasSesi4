5c09a224e5c7a5b02077c41394c5e1d26f2df71d
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2022-12-02 04:23:45 +0700

    Initial Commit

---
+++ TugasSesi4/TugasSesi4App.swift
@@ -0,0 +1,17 @@
+//
+//  TugasSesi4App.swift
+//  TugasSesi4
+//
+//  Created by ihsan.satriawan on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TugasSesi4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
